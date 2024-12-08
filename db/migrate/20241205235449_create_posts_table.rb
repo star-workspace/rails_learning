b0569d8c798964f7de8e2191d187bf0b67c18d7e
star-workspace <ghost2child@gmail.com>
2024-12-08 21:46:43 +0900

    postsテーブル追加

--- db/migrate/20241205235449_create_posts_table.rb
+++ db/migrate/20241205235449_create_posts_table.rb
@@ -1,9 +1,8 @@
 class CreatePostsTable < ActiveRecord::Migration[8.0]
   def change
-    create_table :posts_tables do |t|
+    create_table :posts do |t|
       t.string :title
       t.text :description
-      
       t.timestamps
     end
   end
