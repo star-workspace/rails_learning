5a7cec8043414fb2f773cd8a1806be15b0c32e5b
star-workspace <ghost2child@gmail.com>
2024-12-11 08:49:40 +0900

    post作成機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,24 @@ class PostsController < ApplicationController
   def show
   end
 
+  def new
+  end
+
+  def create
+    post = Post.build(posts_params)
+
+    Rails.logger.debug "#{post.inspect}"
+
+    post.save!
+
+    redirect_to posts_path
+  end
+
   def edit
+  end
+
+  private
+  def posts_params
+    params.permit(:title, :description)
   end
 end
